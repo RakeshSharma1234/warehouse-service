bb97c889b3aae7e7bfecf8d3129b6451b5a05716
Rakesh Sharma <rakesh.sharma04@nagarro.com>
2024-08-11 18:19:20 +0530

    Changes

--- src/test/java/com/company/warehouseservice/WarehouseServiceApplicationTests.java
+++ src/test/java/com/company/warehouseservice/WarehouseServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.company.warehouseservice;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,15 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WarehouseServiceApplicationTests {
   @Test
   void contextLoads() {
-  }
-
-  /**
-   * Test to ensure that the main method starts the application without exceptions.
-   */
-  @Test
-  void testMain() {
-    WarehouseServiceApplication.main(new String[] {});
-    Assertions.assertThatNoException();
-    // If the application fails to start, this test will fail
   }
 }
